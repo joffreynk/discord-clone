dcfc52c14a02f8fb48ce456ef26189321f15a173
joffreynk <joyjoffrey@gmail.com>
2023-09-05 22:54:44 +0200

    add find and create user profile

---
+++ lib/serverActions/profileActions.ts
@@ -0,0 +1,30 @@
+'use server';
+
+import { currentUser, redirectToSignIn } from "@clerk/nextjs"
+import { dbConnection } from "@/lib/dbConnection";
+
+const findAndCreateProfile = async()=> {
+  const user = await currentUser();
+
+  if(!user) return redirectToSignIn();
+
+
+  const profile = await dbConnection.profile.findUnique({
+    where: {
+      userId: user.id
+    }
+  });
+
+  if(profile) return profile;
+
+  const newProfile = await dbConnection.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress
+    }
+  });
+
+  return newProfile;
+}
